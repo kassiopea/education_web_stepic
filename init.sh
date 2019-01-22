35e5494996458c3fbcb4627285ea6a96a48b7651
kassiopea <fotograf.anastasia@gmail.com>
2019-01-22 19:53:58 +0300

    1.9 final init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello
 sudo /etc/init.d/nginx restart
-# sudo service gunicorn restart
 gunicorn -b 0.0.0.0:8080 hello:app
