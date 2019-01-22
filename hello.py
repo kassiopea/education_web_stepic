64fd87640a8c77e96d6b255138787120d43ffe21
kassiopea <fotograf.anastasia@gmail.com>
2019-01-22 19:16:20 +0300

    12

--- hello.py
+++ hello.py
@@ -12,4 +12,4 @@ def app(environ, start_response):
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers )
     # return [ body ]
-return body
+    return body
