91b006f8bd04c734cbeaf3d27c99d84bc5967988
kassiopea <fotograf.anastasia@gmail.com>
2019-01-22 19:52:26 +0300

    1.9 final

--- hello.py
+++ hello.py
@@ -1,15 +1,9 @@
 def app(environ, start_response):
     status = '200 OK'
-    headers = [
-            ('Content-Type', 'text/plain')
-            ]
-    # d = parse_qs(environ['QUERY_STRING'])
-    # print(d)
-    # body = []
-    # for key, value in d:
-    #     body.append(key " = " + value)
+    headers = [('Content-Type', 'text/plain')]
 
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+
     start_response(status, headers )
-    # return [ body ]
+
     return body
