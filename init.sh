d7b3c0d6e59d7873f9061edd270902c4320a3bc6
kassiopea <fotograf.anastasia@gmail.com>
2019-01-22 19:15:16 +0300

    12

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello
 sudo /etc/init.d/nginx restart
 # GUNICORN_CMD_ARGS="--bind=127.0.0.1 --workers=3" gunicorn hello:app
 # gunicorn -b 0.0.0.0:8080 wsgi
