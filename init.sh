f06b90c4d5f867dc43c93c70b5533208523e5274
kassiopea <fotograf.anastasia@gmail.com>
2019-01-22 18:18:04 +0300

    1.9 edit nginx config and init

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 # GUNICORN_CMD_ARGS="--bind=127.0.0.1 --workers=3" gunicorn hello:app
 # gunicorn -b 0.0.0.0:8080 wsgi
-sudo gunicorn -b 0.0.0.0:8080
-# gunicorn -c hello:app
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+# sudo gunicorn -b 0.0.0.0:8080
+gunicorn -c hello:app
+# sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
