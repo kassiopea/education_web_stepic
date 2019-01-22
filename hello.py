3ae48bcf248866d898c533f7908f45a18fa4d851
kassiopea <fotograf.anastasia@gmail.com>
2019-01-22 17:33:20 +0300

    1.9 исправ. hello.py

--- hello.py
+++ hello.py
@@ -3,12 +3,13 @@ def application(environ, start_response):
     headers = [
             ('Content-Type', 'text/plain')
             ]
-    d = parse_qs(environ['QUERY_STRING'])
+    # d = parse_qs(environ['QUERY_STRING'])
 
-    body = []
-    for key, value in d:
-        body.append(key " = " + value)
+    # body = []
+    # for key, value in d:
+    #     body.append(key " = " + value)
 
+    body = "\r\n".join(environ['QUERY_STRING'].split("&"))
     start_response(status, headers )
     # return [ body ]
 
