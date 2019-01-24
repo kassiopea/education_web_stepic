422424ccdc0c49419b70cafcf711a44cb2fc5b18
kassiopea <fotograf.anastasia@gmail.com>
2019-01-24 16:16:01 +0300

    2.1 init

--- init.sh
+++ init.sh
@@ -7,6 +7,5 @@ sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi.conf
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django.conf
 
 sudo /etc/init.d/nginx restart
-sudo python3 manage.py runserver 0:8000
-# sudo gunicorn -c /etc/gunicorn.d/test-django.conf ask.wsgi:application
-# sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+# sudo python3 manage.py runserver 0:8000
+sudo gunicorn -c /etc/gunicorn.d/test-django.conf ask.wsgi:application
