467239e23cd9f96b61957b1f2528adb1e6c398ca
kassiopea <fotograf.anastasia@gmail.com>
2019-01-29 19:34:21 +0300

    2.5 init

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # sudo apt-get install python3.5
 # sudo apt-get install gunicorn
-pip3 install Django==2.0.0
+pip3 install Django==2.0.7
 # sudo pip3 install --upgrade django
 # pip install Django==2.0.7
 sudo rm /etc/nginx/sites-enabled/default
@@ -13,6 +13,8 @@ sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi.conf
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django.conf
 
 sudo /etc/init.d/nginx restart
+sudo python3 manage.py makemigration
+sudo python3 manage.py migrate
 sudo python3 manage.py runserver 0:8000
 
 # gunicorn -c /etc/gunicorn.d/test-django.conf ask.wsgi:application
