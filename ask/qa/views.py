694176d7a6109f9948118e39bd9fdf555497b926
kassiopea <fotograf.anastasia@gmail.com>
2019-01-29 20:57:24 +0300

    2.5 made popular

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -10,35 +10,40 @@ from .models import Question
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-def pagInt(request):
+def pagInt(request, questions, title):
     try:
         page = int(request.GET.get("page"))
     except ValueError:
         page = 1
     except TypeError:
         page = 1
-    return page
-
-def newQuestions(request):
-    page = pagInt(request)
-
-    questions = Question.objects.new()
     paginator = Paginator(questions, 10)
-
     page = paginator.page(page)
 
-    return render(request, 'new_questions.html',
-                  {'title': 'Latest',
+    # return page
+    return render(request, 'questions.html',
+                  {'title': title,
                    'paginator': paginator,
                    'questions': page.object_list,
                    'page': page,
                    'user': request.user,
                    'session': request.session, })
 
-# def popular(request):
-#     try:
-#         page = int(request.GET.get("page"))
-#     except ValueError:
-#         page = 1
-#     except TypeError:
-#         page = 1
+def newQuestions(request):
+    questions = Question.objects.new()
+    page = pagInt(request, questions, 'Latest')
+    # paginator = Paginator(questions, 10)
+
+    # page = paginator.page(page)
+
+    # return render(request, 'questions.html',
+    #               {'title': 'Latest',
+    #                'paginator': paginator,
+    #                'questions': page.object_list,
+    #                'page': page,
+    #                'user': request.user,
+    #                'session': request.session, })
+
+def popular(request):
+    questions = Question.objects.popular()
+    page = pagInt(request, questions, 'Popular')
