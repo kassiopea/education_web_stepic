60fc6e9b3b35789a391e97e88b30fa2546b6c018
kassiopea <fotograf.anastasia@gmail.com>
2019-01-29 18:55:12 +0300

    2.5 add urls.py module view

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from qa.views import test
+from qa.views import test, newquestions
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
